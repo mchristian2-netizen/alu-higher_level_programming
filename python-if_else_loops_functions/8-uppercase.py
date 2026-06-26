35e55f7e1f1d092b168eb04214a140cab2a88745
mchristian2-netizen <mchristian2@alustudent.com>
2026-06-26 01:44:00 -0700

    Add if/else/loops/functions tasks

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for ch in str:
+        if 'a' <= ch <= 'z':
+            result += chr(ord(ch) - 32)
+        else:
+            result += ch
+    print(result)
