2088355a61bb3dbc82bf8b3a2d140b6787039d3b
mchristian2-netizen <mchristian2@alustudent.com>
2026-07-14 04:21:54 -0700

    Add python-classes tasks 0-6

---
+++ python-classes/0-square.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""Empty class Square that defines a square"""
+class Square:
+    pass
---
+++ python-classes/1-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Class Square with private size"""
+class Square:
+    def __init__(self, size):
+        self.__size = size
