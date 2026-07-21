1f26980eed99480975b04dcd69114fe336e81a97
mchristian2-netizen <mchristian2@alustudent.com>
2026-07-21 00:54:00 -0700

    Fix LockedClass: add docstrings and shebang

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+Module for a LockedClass that prevents dynamic attribute creation.
+"""
+
+
 class LockedClass:
+    """
+    A class that only allows the 'first_name' attribute to be set dynamically.
+    Uses __slots__ to restrict attribute creation.
+    """
     __slots__ = ['first_name']
